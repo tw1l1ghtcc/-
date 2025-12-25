5b22d64e6b1f78854f94339008ae1f4a00f0c1ca
tw1l1ghtcc <b1216831200@163.com>
2025-12-25 17:15:33 +0800

    Update 表.洗盘子.cpp

--- 表.洗盘子.cpp
+++ 表.洗盘子.cpp
@@ -1,37 +1,47 @@
 #include <iostream>
 using namespace std;
-const int MAX_QUEUE = 200000; // 队列足够大
-int pigs[MAX_QUEUE];
-int qsize = 0;
+
+const int MAX_QUEUE = 200000;
 const int MAX_LUCKY = 3000;
+
+int pigs[MAX_QUEUE];
 int lucky[MAX_LUCKY];
-int main() {
-    int T;
-    cin >> T;
-    int queries[305];
-    for (int i = 0; i < T; i++) {
-        cin >> queries[i];
-    }
+int qsize = 0;
+
+void generate_lucky() {
     for (int i = 2; i <= MAX_QUEUE; i++) {
         pigs[qsize++] = i;
     }
+
     int count = 0;
-    int head = 0; // 队首索引
+    int head = 0;
+
     while (count < MAX_LUCKY) {
-        int val = pigs[head];       // 队首小猪
+        int val = pigs[head];
         lucky[count++] = val;
 
-        // 原地删除队首之后每隔 val 个小猪
-        int index = head + 1;       // 新队列写入位置
+        int index = head + 1;
         for (int i = head + 1; i < qsize; i++) {
             if ((i - head) % val != 0) {
                 pigs[index++] = pigs[i];
             }
         }
-        qsize = index;              // 更新队列长度
-        head++;                     // 队首出队
+        qsize = index;
+        head++;
     }
-    // 输出查询结果
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    int queries[305];
+    for (int i = 0; i < T; i++) {
+        cin >> queries[i];
+    }
+
+    generate_lucky();
+
     for (int i = 0; i < T; i++) {
         cout << lucky[queries[i] - 1] << endl;
     }
