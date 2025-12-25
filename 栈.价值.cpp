8ed17c864fe6a629352604dddb45471c869ac817
tw1l1ghtcc <b1216831200@163.com>
2025-12-25 19:11:38 +0800

    Update 栈.价值.cpp

--- 栈.价值.cpp
+++ 栈.价值.cpp
@@ -2,18 +2,34 @@
 #include <algorithm>
 using namespace std;
 
+const int MAXN = 100001;
+
+// 全局数组，避免栈溢出
+int a[MAXN];
+long long prefix_sum[MAXN];
+int st[MAXN];
+
 long long calcMaxValue(int n) {
+
+    // 1. 计算前缀和
     prefix_sum[0] = 0;
     for (int i = 1; i <= n; i++) {
         prefix_sum[i] = prefix_sum[i - 1] + a[i - 1];
     }
 
+    // 2. 单调栈
     int top = 0;
     long long max_value = 0;
 
-    for (int i = 0; i < n; ++i) {
+    // 3. 主循环
+    for (int i = 0; i < n; i++) {
+
+        // 当当前元素更小，弹栈
         while (top > 0 && a[st[top - 1]] >= a[i]) {
-            int idx = st[--top];
+
+            int idx = st[top - 1];
+            top--;
+
             int right = i;
 
             int left;
@@ -30,11 +46,18 @@ long long calcMaxValue(int n) {
                 max_value = value;
             }
         }
-        st[top++] = i;
+
+        // 当前下标入栈
+        st[top] = i;
+        top++;
     }
 
+    // 4. 清空栈中剩余元素
     while (top > 0) {
-        int idx = st[--top];
+
+        int idx = st[top - 1];
+        top--;
+
         int right = n;
 
         int left;
@@ -56,14 +79,13 @@ long long calcMaxValue(int n) {
 }
 
 int main() {
-    int N;
-    cin >> N;
+    int n;
+    cin >> n;
 
-    int a[100000];
-    for (int i = 0; i < N; ++i) cin >> a[i];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
 
-    cout << calcMaxValue(a, N) << endl ;
+    cout << calcMaxValue(n) << endl;
     return 0;
 }
-
-
